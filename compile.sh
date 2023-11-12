fa1bd5332fd1b91d04d00f200610dab789bea87b
probonopd <probonopd@users.noreply.github.com>
2023-11-12 11:49:46 +0100

    -Xs

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-/usr/bin/fpc -Tlinux -Px86_64 -MObjFPC -Scghi -CX -Cgstatic -O3 -XX -l -vewnhibq -Filib/x86_64-linux -Fuunits -Fu. -FUlib/x86_64-linux -FEbin/Linux -obin/Linux/MDX_PerfConv -dCMDLINE MDX_PerfConv.lpr
+/usr/bin/fpc -Tlinux -Px86_64 -MObjFPC -Scghi -CX -O3 -XX -l -vewnhibq -Filib/x86_64-linux -Fuunits -Fu. -FUlib/x86_64-linux -FEbin/Linux -obin/Linux/MDX_PerfConv -dCMDLINE -Xs MDX_PerfConv.lpr
